1361d8ee72446e39bca218dd126fac916386e2da
tedbeast <tedbalashov@gmail.com>
2022-02-08 11:26:34 -0500

    added HelloRevature

---
+++ HelloRevature.java
@@ -0,0 +1,26 @@
+public class HelloRevature {
+
+	public static void main(String[] args){
+		
+		System.out.println("Hello Revature!");
+		
+	}
+
+}
+
+/*
+	Java compile to bytecode which is run within the Java Virtual Machine
+	Virtual Machine: virtual machine is a walled-off operating system within an operating system
+	Because we are running in the Java Virtual Machine, Java is what we call 
+	"Write once, run anywhere"
+	The JVM is independent of the operating system: so we can use Java the same way on Windows, Mac, Linux
+
+	Automatic garbage collection
+	
+	Very safe, reusable
+	
+	Java is one of those things that's popular because it's popular
+	(because Java has a very intense amount of standardization thanks to the JVM)
+
+	The environment that Java exists in is consistent everywhere because of the JRE, JVM
+*/
