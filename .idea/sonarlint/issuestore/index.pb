40edc274e80c53a4cb60099a1b53a53152996c30
tedbeast <tedbalashov@gmail.com>
2022-02-14 12:29:22 -0500

    started sql file

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+Q
+!src/main/java/cookies/Cookie.java,b\c\bcf2fc3f2027beb5e1a8a578730d53ee2efc47cf
+M
+src/main/java/zoo/Animal.java,7\b\7beb88b571df298fc52ed785a315c195b9a98f0c
+Z
+*src/main/java/cookies/CookieArrayList.java,e\b\eb4fe107887fdb7c53f7795c9f6dbf03cde58c90
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+S
+#src/main/java/zoo/AnimalBranch.java,e\c\ec06d0accc2144976bd4bbfeb9abf8726b20a871
+Q
+!src/main/java/zoo/AnimalTree.java,3\c\3c79ad84b86f0dda8f8c139e97cf43eeb16ab170
---
+++ museumSQL.sql
@@ -0,0 +1,118 @@
+/*
+Why do we use the cloud?
+not spend so much on server resources
+Microsoft Azure is among many cloud providers -
+the real big player is AWS, but its market share is shrinking because of the growth in Azure and GCP
+why cloud?
+global availability - regional data centers
+high availability - region pairing
+scalability
+	if we have high usage at one point, we can auto scale up or auto scale down
+	which means our money is being used more efficiently
+
+Infrastructure as a service
+	purchasing a virtual machine
+Platform as a service
+	azure devops
+		azure devops just runs some script that we feed it builds our project (maven) for us
+	azure sql
+Software as a service
+	salesforce, servicenow, pega
+
+SQL?
+Structured Query Language
+Relational Database
+whats that?
+Items in one table can be related to items in another
+There are many flavors of SQL - because native SQL is pretty simplistic,
+many versions have been created to add new features (mysql, postgres, azuresql)
+
+NoSQL
+MongoDB, Cosmos, Cassandra
+
+datatypes:
+numeric:
+int
+bigint
+float
+money
+
+strings:
+char
+varchar
+
+binary:
+bit
+blob
+clob
+file
+
+others:
+date
+datetime
+
+in dbeaver, running single line at cursor: ctrl enter
+running whole script: alt x
+
+issues with non-relational organization
+	1. redundancy
+	2. what if a value needs to change?
+
+constraints
+primary key:
+	the primary key MUST be unique
+	the primary key MUST exist (not null)
+	primary keys are indexed by default
+
+foreign keys:
+	MUST refer to an existing primary key
+
+What we did with reducing redundancy between tables while maintaining the
+dependency between painting and artist tables will relate to a topic
+which we will talk about later - normalization
+
+multiplicity - we can say that x amount of a correspond to y amount of b
+for example, an artist many have many paintings
+(many to one)
+many paintings can be exhibited (over a single paintings lifetime) at many museums
+(many to many)
+a single thing might map to another single thing
+(one to one)
+
+entity relational diagrams - not something we'll look into but thanks for bringing it up
+
+*/
+
+DROP TABLE IF EXISTS painting;
+DROP TABLE IF EXISTS artist;
+
+
+CREATE TABLE artist(
+                       artist_id int PRIMARY KEY,
+                       name varchar(40) NOT NULL
+);
+
+CREATE TABLE painting(
+                         artist_id int,
+                         title varchar(40),
+                         url varchar(40),
+                         genre varchar(40),
+                         year_made INT,
+                         FOREIGN KEY (artist_id) REFERENCES artist(artist_id)
+);
+
+INSERT INTO artist (artist_id, name)
+VALUES
+    (0, 'Wasilly Kandinsky'),
+    (1, 'Paul Klee');
+
+INSERT INTO painting (artist_id, title, url, genre, year_made)
+VALUES
+    (0, 'Composition IX', null, 'Abstract', 1939),
+    (1, 'Magic Fishes', null, 'Abstract', 1938);
+
+SELECT * FROM painting;
+SELECT title, year_made FROM painting;
+SELECT title, name FROM (painting join artist on painting.artist_id = artist.artist_id);
+
+
